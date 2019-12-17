3365b37ae5ffae649a3c352a22db0b64c4dc3cd2
Bbabud <56270250+Bbabud@users.noreply.github.com>
2019-12-17 03:34:23 +0100

    Updates

---
+++ precache-manifest.72acd2ac92eab174996f347cc496957e.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e39dfcb488ab4c4e2f6718dd7d0cfc5",
+    "url": "/choreograpify_v2/index.html"
+  },
+  {
+    "revision": "baedec255a69aff02f8b",
+    "url": "/choreograpify_v2/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "06b6db6e76728f297428",
+    "url": "/choreograpify_v2/static/css/main.478d8d4a.chunk.css"
+  },
+  {
+    "revision": "baedec255a69aff02f8b",
+    "url": "/choreograpify_v2/static/js/2.76e3fcc5.chunk.js"
+  },
+  {
+    "revision": "e4ff55ceb134e48460229b253f700caa",
+    "url": "/choreograpify_v2/static/js/2.76e3fcc5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "06b6db6e76728f297428",
+    "url": "/choreograpify_v2/static/js/main.96a1e1cd.chunk.js"
+  },
+  {
+    "revision": "569a43bea0513ef67a30",
+    "url": "/choreograpify_v2/static/js/runtime-main.81c0081a.js"
+  },
+  {
+    "revision": "c689533253b4d527de2b06548c862163",
+    "url": "/choreograpify_v2/static/media/BasicBackward.c6895332.svg"
+  },
+  {
+    "revision": "e543a3d5b896b2efc849b7d7a91ed9df",
+    "url": "/choreograpify_v2/static/media/BasicForward.e543a3d5.svg"
+  },
+  {
+    "revision": "1b119f5f3099f264fd5aa4a449c0bb56",
+    "url": "/choreograpify_v2/static/media/ChasseFromPPLady.1b119f5f.svg"
+  },
+  {
+    "revision": "b6c0fcd1fee5eae10f3c76a6c4c539a5",
+    "url": "/choreograpify_v2/static/media/ChasseFromPPMan.b6c0fcd1.svg"
+  },
+  {
+    "revision": "1703c02d5d65b55b9fc1baf6bc9e5991",
+    "url": "/choreograpify_v2/static/media/ClosedChangeLFBackward.1703c02d.svg"
+  },
+  {
+    "revision": "9b5e0165adbb7a832d3c149eb721b109",
+    "url": "/choreograpify_v2/static/media/ClosedChangeLFForward.9b5e0165.svg"
+  },
+  {
+    "revision": "3a779fdbf89a5251634d8e2735b6ee19",
+    "url": "/choreograpify_v2/static/media/ClosedChangeRFBackward.3a779fdb.svg"
+  },
+  {
+    "revision": "bf73d3f40562d2b2af77d2d54befd2ca",
+    "url": "/choreograpify_v2/static/media/ClosedChangeRFForward.bf73d3f4.svg"
+  },
+  {
+    "revision": "deff9929e8b21697bd509a7ada8a2c2f",
+    "url": "/choreograpify_v2/static/media/NaturalSpinTurnBackward.deff9929.svg"
+  },
+  {
+    "revision": "732f4e2d1272cc189d5f90d8533f079d",
+    "url": "/choreograpify_v2/static/media/NaturalSpinTurnForward.732f4e2d.svg"
+  },
+  {
+    "revision": "ee5be4e29b99ab7875e7f14694c1f71b",
+    "url": "/choreograpify_v2/static/media/NaturalTurnBackward.ee5be4e2.svg"
+  },
+  {
+    "revision": "97cc53f35acf5777d330c647a44b2323",
+    "url": "/choreograpify_v2/static/media/NaturalTurnForward.97cc53f3.svg"
+  },
+  {
+    "revision": "b679bf534cd5f26d209a7cac2ba46d35",
+    "url": "/choreograpify_v2/static/media/OutsideChangeLady.b679bf53.svg"
+  },
+  {
+    "revision": "52145d140d030456fab8ef4b6bdd62bd",
+    "url": "/choreograpify_v2/static/media/OutsideChangeMan.52145d14.svg"
+  },
+  {
+    "revision": "4479a001896b4568d7972fec9e0963d3",
+    "url": "/choreograpify_v2/static/media/ProgressiveChasseToRightLady.4479a001.svg"
+  },
+  {
+    "revision": "cc302b154dcce8c6e3e47f76d3e52d85",
+    "url": "/choreograpify_v2/static/media/ProgressiveChasseToRightMan.cc302b15.svg"
+  },
+  {
+    "revision": "057edc860c61a5f856037ba7875f46e1",
+    "url": "/choreograpify_v2/static/media/ReverseTurnBackward.057edc86.svg"
+  },
+  {
+    "revision": "070dd55cd1447f370e4f594e2cb42f6b",
+    "url": "/choreograpify_v2/static/media/ReverseTurnForward.070dd55c.svg"
+  },
+  {
+    "revision": "bbab1d7e6fd3baf5edbd86a58c020355",
+    "url": "/choreograpify_v2/static/media/TurningLockToLeftLady.bbab1d7e.svg"
+  },
+  {
+    "revision": "d3dac34ae67247a6cfcdd4af122cd97c",
+    "url": "/choreograpify_v2/static/media/TurningLockToLeftMan.d3dac34a.svg"
+  },
+  {
+    "revision": "ff60e931a85379f7d917c32c1252b46e",
+    "url": "/choreograpify_v2/static/media/WhiskBackward.ff60e931.svg"
+  },
+  {
+    "revision": "4f68152117ab573fde56d4d0a72b05b1",
+    "url": "/choreograpify_v2/static/media/WhiskForward.4f681521.svg"
+  }
+]);
